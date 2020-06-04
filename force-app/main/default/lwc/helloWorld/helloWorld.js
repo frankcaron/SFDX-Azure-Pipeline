0c089be46fb7e04a2185d5c2dc45100b35d4bdf8
Frank Caron <frank@frankcaron.com>
2020-06-04 16:28:05 -0400

    Update to the component language

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,3 +1,17 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
+import helloWorld from '@salesforce/apex/apexMaster.getHelloWorld';
 
-export default class HelloWorld extends LightningElement {}
+export default class HelloWorld extends LightningElement {
+    @track valueToDisplay;
+
+    @wire(helloWorld)
+    helloWorldBlurb({ error, data }) {
+        if (data) {
+            this.valueToDisplay = data;
+        } else if (error) {
+            this.valueToDisplay = "Someone doesn't know how to write LWC properly.";
+        }
+    }
+
+   
+}
